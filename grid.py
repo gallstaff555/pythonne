cf63ccf4c60517207d1dd0b3605aabb6e37c1c99
David <davidsolo555@hotmail.com>
2023-08-06 15:30:52 -0700

    Add comment and cleanup

--- grid.py
+++ grid.py
@@ -1,10 +1,6 @@
 import pygame
 from config import Config
 
-TILE_SIZE = 32
-X_OFFSET = 7
-Y_OFFSET = 180
-
 cfg = Config()
 
 class Grid():
@@ -14,12 +10,7 @@ class Grid():
         self.right = (cfg.FULL_WIDTH, cfg.HALF_HEIGHT)
         self.down = (cfg.HALF_WIDTH, cfg.FULL_HEIGHT)
 
-        # origin at left
-        # self.orgin = (0, cfg.HALF_HEIGHT)
-        # top = (cfg.HALF_WIDTH, 0)
-        # right = (cfg.FULL_WIDTH, cfg.HALF_HEIGHT)
-        # down = (cfg.HALF_WIDTH, cfg.FULL_HEIGHT)
-
+        #define outer corners of grid
         self.map_corners = [
             pygame.math.Vector2(self.origin),
             pygame.math.Vector2(self.left), 
@@ -30,21 +21,7 @@ class Grid():
         self.x_axis = (self.map_corners[2] - self.origin) / cfg.COL
         self.y_axis = (self.map_corners[1] - self.origin) / cfg.ROW
 
-
-        # origin at self.left
-        # self.x_axis = (self.map_corners[1] - self.map_corners[0]) / cfg.COL
-        # self.y_axis = (self.map_corners[3] - self.map_corners[0]) / cfg.ROW
-
-
-        print(f"x_axis: {self.x_axis}")
-        print(f"y_axis: {self.y_axis}")
-
-        # print("corners:")
-        # print(self.orgin)
-        # print(self.left)
-        # print(right)
-        # print(down)
-
+    # Display tile currently moused over
     def get_mouseover_tile(self, mouse):
         point_to_grid = self.get_point_to_grid((self.x_axis, self.y_axis))
         m_pos = pygame.mouse.get_pos()
@@ -52,40 +29,17 @@ class Grid():
         m_col, m_row = int(m_grid_pos[0]), int(m_grid_pos[1])
         print(f"m_col: {m_col}, m_row: {m_row}")
         
-
+    # Convert tile coordinates to mouse coordinate
     def transform_to_point(self, point, mat2x2):
         x = point[0] * mat2x2[0][0] + point[1] * mat2x2[1][0]
         y = point[0] * mat2x2[0][1] + point[1] * mat2x2[1][1]
         return pygame.math.Vector2(x, y)
     
+    # Conversion for mouse coord to tile coord
     def get_point_to_grid(self, axes):
         a, b, c, d = axes[0].x, axes[0].y, axes[1].x, axes[1].y
         det = 1 / (a*d - b*c)
         return [(d*det, -b*det), (-c*det, a*det)]
-
-    def test(self):
-        point1 = (9,0)
-        p_position1 = self.transform_to_point((point1[0] + .5, point1[1] + .5), (self.x_axis, self.y_axis)) + self.origin
-    
-        #print(f"point1: {point1}, p_position: {p_position1}")
-        #print(p_position2)
-
         
 
     
-    
-    
-    
-    
-    
-    
-    # def mouse_to_grid(self, x, y):
-    #     world_x = x - 
-    # def iso_to_cart(self, mouse_x, mouse_y):
-    #     cart_y = (2 * mouse_y - mouse_x) / 2
-    #     cart_x = cart_y + mouse_x
-
-    #     grid_x = int(cart_x // TILE_SIZE)
-    #     grid_y = int(cart_y // TILE_SIZE)
-
-    #     print(f"grid_x: {grid_x}, grid_y: {grid_y}")
