56b09ddd5eecb9f965dda7b92e7511216c27b196
David <davidsolo555@hotmail.com>
2023-08-06 14:58:00 -0700

    origin is now left corner

--- grid.py
+++ grid.py
@@ -9,20 +9,25 @@ cfg = Config()
 
 class Grid():
     def __init__(self):
-        origin = (cfg.HALF_WIDTH, 0)
-        left = (0, cfg.HALF_HEIGHT)
+        # origin = (cfg.HALF_WIDTH, 0)
+        # left = (0, cfg.HALF_HEIGHT)
+        # right = (cfg.FULL_WIDTH, cfg.HALF_HEIGHT)
+        # down = (cfg.HALF_WIDTH, cfg.FULL_HEIGHT)
+
+        origin = (0, cfg.HALF_HEIGHT)
+        top = (cfg.HALF_WIDTH, 0)
         right = (cfg.FULL_WIDTH, cfg.HALF_HEIGHT)
         down = (cfg.HALF_WIDTH, cfg.FULL_HEIGHT)
 
         self.map_corners = [
             pygame.math.Vector2(origin),
-            pygame.math.Vector2(right), 
-            pygame.math.Vector2(left),
+            pygame.math.Vector2(top), 
+            pygame.math.Vector2(right),
             pygame.math.Vector2(down)
         ]
 
         self.x_axis = (self.map_corners[1] - self.map_corners[0]) / cfg.COL
-        self.y_axis = (self.map_corners[2] - self.map_corners[0]) / cfg.ROW
+        self.y_axis = (self.map_corners[3] - self.map_corners[0]) / cfg.ROW
 
         print(f"x_axis: {self.x_axis}")
         print(f"y_axis: {self.y_axis}")
@@ -34,25 +39,40 @@ class Grid():
         # print(down)
 
     def get_mouseover_tile(self, mouse):
-        x, y = mouse.get_pos()
+        (x, y) = mouse.get_pos()
         print(f"x:{x}, y:{y}")
-        print(self.transform((x, y), (self.x_axis, self.y_axis)))
-        #self.iso_to_cart(x, y)
+        
+        self.test()
+        # point_to_grid = self.coord_to_grid((self.x_axis, self.y_axis))
+        # mouse_grid_pos = self.transform(pygame.math.Vector2((x, y)) - self.map_corners[0], point_to_grid)
+        # print(point_to_grid)
+        # print(mouse_grid_pos)
+        # print()
 
-    def transform(self, point, mat2x2):
+    def transform_to_point(self, point, mat2x2):
         x = point[0] * mat2x2[0][0] + point[1] * mat2x2[1][0]
         y = point[0] * mat2x2[0][1] + point[1] * mat2x2[1][1]
-        return pygame.math.Vector2(x, y) + self.map_corners[0]
+        return pygame.math.Vector2(x, y)
     
-    # def test(self):
-        # point1 = (0,0)
-        # p_position1 = self.transform((point1[0] + .5, point1[1] + .5), (self.x_axis, self.y_axis)) + self.map_corners[0]
-        # point2 = (9,9)
-        # p_position2 = self.transform((point2[0] + .5, point2[1] + .5), (self.x_axis, self.y_axis)) + self.map_corners[0]
-        
-        # print(p_position1)
-        # print(p_position2)
+    def transform_to_grid(self, m):
+        a, b, c, d = m[0].x, m[0].y, m[1].x, m[1].y
+        det = 1 / (a*d - b*c)
+        return [(d*det, -b*det), (-c*det, a*det)]
+
+    def test(self):
+        point1 = (9,0)
+        p_position1 = self.transform_to_point((point1[0] + .5, point1[1] + .5), (self.x_axis, self.y_axis)) + self.map_corners[0]
     
+        #print(f"point1: {point1}, p_position: {p_position1}")
+        #print(p_position2)
+
+        point_to_grid = self.transform_to_grid((self.x_axis, self.y_axis))
+        print(point_to_grid)
+        m_pos = pygame.mouse.get_pos()
+        m_grid_pos = self.transform_to_point(pygame.math.Vector2(m_pos) - self.map_corners[0], point_to_grid)
+        m_col, m_row = int(m_grid_pos[0]), int(m_grid_pos[1])
+        print(f"m_col: {m_col}, m_row: {m_row}")
+
     
     
     
